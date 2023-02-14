eab9455d37c6898e9d0130cafd3860a6d9b2cb97
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-14 18:15:21 +0530

    Update main.js

--- 17 Classes/js/main.js
+++ 17 Classes/js/main.js
@@ -1,1 +1,18 @@
 // Javascript Classes
+
+class Pizza {
+    constructor() {
+        this.size = "medium";
+        this.crust = "original";
+    }
+    bake() {
+        console.log(`Baking a ${this.size} ${this.crust} crust Pizza!`)
+    }
+}
+
+// console.log(Pizza);          This will give the entire code inside the class in output!
+// console.log(Pizza.bake());   Uncaught TypeError: Pizza.bake is not a function!
+// console.log(Pizza.bake);     Undefined!
+
+const myPizza = new Pizza();
+myPizza.bake();
