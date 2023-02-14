36e5fc26c4b85fa8dbad4101a43a64c854fa6212
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-14 17:03:40 +0530

    Chapter 9

---
+++ Chapter 9 Ternary Operator/js/main.js
@@ -0,0 +1,28 @@
+// Conditionals: Ternary Operator
+
+// Syntax:
+// Condition ? ifTrue : ifFalse
+
+// Example 1
+
+// let jolpan = "Sira Jolpan with doi & cream";
+// let response = jolpan ? "Yes, we have Jolpan" : "Sorry, no jolpan today!";
+
+// console.log(response);
+
+
+//  Example 2
+
+// let jolpan = "Sira Jolpan with doi & cream";
+// let isCustomerBanned = false;
+// let jolpanAccess = isCustomerBanned ? "Sorry, no jolpan for you!" : jolpan ? `Yes, we have ${jolpan} today` : "Sorry, no jolpan today";
+
+// console.log(jolpanAccess);
+
+
+// Example 3
+
+let testScore = 89;
+let myGrade = testScore > 90 ? "A" : testScore > 80 ? "B" : testScore > 70 ? "C" : testScore > 60 ? "D" : "F";
+
+console.log(`My test grade is ${myGrade}`);
