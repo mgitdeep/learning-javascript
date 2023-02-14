5a8b724879aab3ceccf3674530fcc06fe79a57e6
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-14 17:01:26 +0530

    Chapter 5

---
+++ Chapter 5 Math Methods/js/main.js
@@ -0,0 +1,27 @@
+// Math Methods and Properties
+console.log(Math.PI);
+console.log(Math.trunc(Math.PI));
+console.log(Math.round(Math.PI));
+console.log(Math.round(3.39));
+console.log(Math.round(3.69));
+console.log(Math.ceil(3.12));          // ceil always round the value UP
+console.log(Math.floor(3.12));          // floor always round the value DOWN
+
+console.log(Math.pow(3, 2));
+console.log(Math.min(3, 2, 1.2));
+console.log(Math.max(3, 2, 1.2));
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+
+console.log(Math.floor(0.21));
+console.log(Math.floor(0.21 * 10));
+
+// Generate random nos from 1 to 10
+
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
