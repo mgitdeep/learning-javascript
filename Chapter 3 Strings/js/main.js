9123394dd0b8b48b83e60a8fbf29bbeb9c921aa3
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-14 16:59:44 +0530

    Chapter 3

---
+++ Chapter 3 Strings/js/main.js
@@ -0,0 +1,31 @@
+// console.log("Hello World!");
+// console.log(typeof "Mondeep");
+// console.log(typeof 36);
+// console.log(typeof true);
+
+// STRINGS
+const myVariable = "Mathematics";
+
+// The LENGTH property
+console.log(myVariable.length);
+console.log("Yogah Karmasu Kaushalam".length);
+
+// String Methods
+console.log(myVariable.charAt(0));
+
+console.log(myVariable.indexOf("Mat"));
+console.log(myVariable.lastIndexOf("at"));
+
+console.log(myVariable.slice(3, 6));
+console.log(myVariable.slice(3));
+
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+
+console.log(myVariable.includes("var"));
+console.log(myVariable.includes("ics"));
+
+console.log(myVariable.split("e"));
+console.log(myVariable.split(""));
+console.log("John,Joe,Dave".split(","));
+console.log("Yogah Karmasu Kaushalam".split(" "));
