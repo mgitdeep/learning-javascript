608f1b2e038fb1b53f12e7b241d3dd32d4e82d9a
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-17 16:17:18 +0530

    Chapter 17

---
+++ 17 Classes/js/main_02.js
@@ -0,0 +1,20 @@
+<html>
+<body>
+    <h2 id = "title">Classes: The Basics</h2>
+     <script type = "text/javascript" >
+         /*
+             Class - Object
+
+             Instance Properties: what they have
+                 - name
+                 - age
+                 - height
+
+             Instance Methods: what they do
+                 - talk
+                 - run
+                 - jump
+         */
+     </script>
+</body>
+</html>
