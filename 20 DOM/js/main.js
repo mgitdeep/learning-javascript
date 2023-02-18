5055d5dccf1169646c0106f07e4d9e84eb7a6799
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-18 12:23:18 +0530

    Chapter 20

---
+++ 20 DOM/js/main.js
@@ -0,0 +1,13 @@
+// DOM - Document Object Model
+
+const view1 = document.getElementById("view1");
+console.log(view1);
+
+const view2 = document.querySelector("#view2");
+console.log(view2);
+
+view1.style.display = "flex";
+view2.style.display = "flex";
+
+const views = document.getElementsByClassName("view");
+console.log(views);
