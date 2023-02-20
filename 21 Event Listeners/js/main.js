a0bda83f2c1aec617d771baa5914de41dde5997a
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-20 08:05:47 +0530

    Chapter 21

--- 21 Event Listeners/js/main.js
+++ 21 Event Listeners/js/main.js
@@ -1,7 +1,26 @@
 // JavaScript Event Listeners
 
-const view = document.querySelector("#view2");
-console.log(view);
-
 view1.style.display = "none";
 view2.style.display = "flex";
+
+const view = document.querySelector("#view2");
+console.log(view);
+const div = view.querySelector("div");
+console.log(div);
+const h2 = div.querySelector("h2");
+console.log(h2);
+
+// now we'll add a Event Listener
+// Syntax: addEventListener(event, function, useCapture)
+
+const doSomething = () => {
+    alert("doing something!");
+};
+
+h2.addEventListener("click", doSomething, false);
+h2.removeEventListener("click", doSomething, false);
+
+h2.addEventListener("click", (Event) => {
+    console.log(Event.target);
+    Event.target.textContent = "Clicked!";
+})
