eb333e1307b672073f4f54cd445f24c769d89f6b
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-20 20:08:32 +0530

    Chapter 22

--- 22 Web Storage API/js/main.js
+++ 22 Web Storage API/js/main.js
@@ -5,5 +5,29 @@
 
 // We do not have to type window. It is implied:
 
-window.alert(window.location);
-alert(location);
+// window.alert(window.location);
+// alert(location);
+
+
+const myArray = ['eat', 'sleep', 'code'];
+const myObject = {
+    name: "Mondeep",
+    hobbies: ['eat', 'sleep', 'code'],
+    logName: function () {
+        console.log(this.name);
+    }
+}
+
+// console.log(myObject)
+// myObject.logName();
+
+localStorage.setItem("mySeesionStore", JSON.stringify(myArray));
+// localStorage.removeItem("mySeesionStore")
+// const key = localStorage.key(0);
+const storeLength = localStorage.length;
+const mySeesionData = JSON.parse(localStorage.getItem("mySeesionStore"));
+// console.log(key);
+console.log(storeLength);
+// console.log(mySeesionData);
+console.log(typeof mySeesionData);
+// localStorage.removeItem("mySeesionStore")
