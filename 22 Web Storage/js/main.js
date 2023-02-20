ff1809e911caf597a6135d8e8ae840c91e17f4b2
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-20 17:48:34 +0530

    Chapter 22

---
+++ 22 Web Storage/js/main.js
@@ -0,0 +1,9 @@
+// Web Storage API
+
+// Not part of the DOM - refers to the Window API
+// Available to OS via the global variable: Window
+
+// We do not have to type window. It is implied:
+
+window.alert(window.location);
+alert(location);
