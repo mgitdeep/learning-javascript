f30bbb77018023898b436d603a3c491b707467a3
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-21 17:42:09 +0530

    Chapter 23

--- 23 Modules/js/main.js
+++ 23 Modules/js/main.js
@@ -1,5 +1,22 @@
 // Modules
 
-import playGuitar from "./guitars.js";
+// import playGuitar from "./guitars.js";
+// import { shredding as shred, plucking as fingurepicking } from "./guitars.js";
 
-console.log(playGuitar());
+// console.log(playGuitar());
+// console.log(shred());
+// console.log(fingurepicking());
+
+import * as Guitars from "./guitars.js";
+
+// console.log(Guitars.playGuitar());              - it won't work since it's set as DEFAULT in guitars.js
+console.log(Guitars.default());
+console.log(Guitars.shredding());
+console.log(Guitars.plucking());
+
+import User from "./user.js";
+
+// console.log(User.default("email@email.com", "Dave"));
+const me = new User("email@email.com", "Mondeep");
+console.log(me);
+console.log(me.greetings());
