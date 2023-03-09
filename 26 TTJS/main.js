5c985ea28a593c37e7a4fddd6536d86336af784e
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-09 15:35:20 +0530

    Updates

--- 26 TTJS/main.js
+++ 26 TTJS/main.js
@@ -532,4 +532,45 @@
 // of milliseconds elapsed since January 1, 1970 00:00:00 UTC
 
 
-console.log(Date.now());
+// console.log(Date.now());
+
+
+// The DOM is the Document Object Model, which deals with the document, 
+// the HTML elements themselves, e.g. document and all traversal you 
+// would do in it, events, etc.
+
+// For Ex: 👨‍🏫
+// change the background color to red
+// document.body.style.background = "red";
+
+
+// The BOM is the Browser Object Model, which deals with browser components 
+// aside from the document, like history, location, navigator and screen 
+// (as well as some others that vary by browser). OR 
+// In simple meaning all the Window operations which comes under BOM are performed 
+// usign BOM 
+
+
+// Let's see more practical on History object 
+
+// Functions alert/confirm/prompt are also a part of BOM: 
+// they are directly not related to the document, 
+// but represent pure browser methods of communicating with the user.
+
+// alert(location.href); // shows current URL
+// if (confirm("Want to Visit ThapaTechnical?")) {
+//   location.href = "https://www.youtube.com/thapatechnical"; // redirect the browser to another URL
+// }
+
+
+
+// Navigate through the DOM 
+
+// 1: document.documentElement
+//       returns the Element that is the root element of the document. 
+// 2: document.head
+// 3: document.body
+// 4: document.body.childNodes (include tab,enter and whiteSpace)
+//   list of the direct children only 
+// 5: document.children (without text nodes, only regular Elements)
+// 6: document.childNodes.length
