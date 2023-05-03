f9bbff34ed253f81d6677df08344de0e93b81ef3
Mondeep Kalita <epicdeep09@gmail.com>
2023-05-03 15:55:45 +0530

    Experimented on Hoisting and Call Stack

---
+++ 30 Namaste JS/v4.js
@@ -0,0 +1,18 @@
+
+getName()
+console.log(x)
+console.log(getName())
+ 
+var x = 3
+
+function getName() {
+    console.log("Namaste JS")
+}
+
+// const getName = () => {
+//     console.log("Namaste JS")
+// }
+
+
+
+
